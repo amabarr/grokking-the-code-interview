12223888fc25cce25bcb346a7de18ea456c32aa3
Amanda Barrafato <amb1047@nyu.edu>
2021-05-30 13:58:04 -0400

    triple sum smaller

---
+++ two_pointers/triple_sum_smaller.js
@@ -0,0 +1,14 @@
+/*
+
+Given an array arr of unsorted numbers and a target sum, count all triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three different indices. Write a function to return the count of such triplets.
+
+
+example:
+[3, 1, 2, 6, 7] target = 10
+answers : [1, 2, 3], [1, 2, 6]
+return 2;
+
+Input: [-1, 0, 2, 3], target=3
+Output: 2
+Explanation: There are two triplets whose sum is less than the target: [-1, 0, 3], [-1, 0, 2]
+*/
